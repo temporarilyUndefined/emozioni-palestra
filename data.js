62f45cd4f372d55ca64a618aad210ec3f6e36d48
temporarilyUndefined <115276090+temporarilyUndefined@users.noreply.github.com>
2022-10-11 02:22:14 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+let text = [
+   {
+    emozioni: ["disagio", "", "", "", "", "", ""],
+    descrizione: ["oggi ho sudato tanto, ma veramente tanto. non so il perche, non ho fatto esercizi diversi, forse era piu caldo. fatto sta che ero zuppo. so che in palestra e normale sudare ma mi ha provocato un po di disagio, niente di eccessivo. ho comunque terminato il mio allenamento tranquillamente", "", "niente di particolarmente emozionante e successo oggi. ho provato un orario diverso dal solito (14:30 invece che 16) forse c'era un po meno gente, oltre questo nessuna differenza.", "", "anche oggi non mi sembra di aver provato particolari emozioni. Mentre tornavo a casa riflettendo mi é sembrato di essere piu rilassato rispetto all'andata", "", ""]
+    },
+    {
+    emozioni: [],
+    descrizione: []
+    }
+]
+
--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-let currentWeek = 0;
 const firstDay = new Date("2022-10-3").getTime()
 const weekms = 604800000
+let currentWeek = Math.floor((Date.now() - firstDay)/weekms) - 1;
 
 function previousweek(){
     if(currentWeek){
         currentWeek-=1;
-        changeWeek();
+        changeWeek(text[currentWeek] ? text[currentWeek] : {emozioni: [], descrizione: []});
     }
 }
 
 function nextweek(){
-    currentWeek +=1
-    changeWeek();
+    currentWeek +=1;
+    changeWeek(text[currentWeek] ? text[currentWeek] : {emozioni: [], descrizione: []});
 }
 
-function changeWeek(){
-    let startDay = firstDay + weekms * currentWeek;
+function changeWeek(data){
+    let startDay = new Date(firstDay + weekms * currentWeek);
+    let endDay = new Date(startDay.getTime() + 518400000)
+    document.getElementById("weekDate").innerText = `${startDay.getDate()}/${startDay.getMonth() + 1}/${startDay.getFullYear()}-${endDay.getDate()}/${endDay.getMonth() + 1}/${endDay.getFullYear()}`
+    let emotionTab = document.getElementsByClassName("tabEm");
+    let descTab = document.getElementsByClassName("tabDesc");
+    let vuoto = ""
     for (let i = 0; i < 7; i++) {
-        
-        
+        emotionTab[i].innerText = `${data.emozioni[i] ? data.emozioni[i] : vuoto}`;
+        descTab[i].innerText = `${data.descrizione[i] ? data.descrizione[i] : vuoto}`;
     }
 }
 
-
+window.onload = function() {
+    changeWeek(text[currentWeek] ? text[currentWeek] : {emozioni: [], descrizione: []})
+    console.log(currentWeek);
+}
