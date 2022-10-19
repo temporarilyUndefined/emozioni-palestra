b318747e82369615192841f22378e5260289c9dd
temporarilyUndefined <115276090+temporarilyUndefined@users.noreply.github.com>
2022-10-19 17:36:54 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,46 @@
 let text = [
    {
     emozioni: ["disagio", "", "", "", "", "", ""],
-    descrizione: ["oggi ho sudato tanto, ma veramente tanto. non so il perche, non ho fatto esercizi diversi, forse era piu caldo. fatto sta che ero zuppo. so che in palestra e normale sudare ma mi ha provocato un po di disagio, niente di eccessivo. ho comunque terminato il mio allenamento tranquillamente", "", "niente di particolarmente emozionante e successo oggi. ho provato un orario diverso dal solito (14:30 invece che 16) forse c'era un po meno gente, oltre questo nessuna differenza.", "", "anche oggi non mi sembra di aver provato particolari emozioni. Mentre tornavo a casa riflettendo mi é sembrato di essere piu rilassato rispetto all'andata", "", ""]
+    descrizione: ["oggi ho sudato tanto, ma veramente tanto. non so il perche, non ho fatto esercizi diversi, forse era piu caldo. fatto sta che ero zuppo. so che in palestra e normale sudare ma mi ha provocato un po di disagio, niente di eccessivo. ho comunque terminato il mio allenamento tranquillamente",
+        "",
+        "niente di particolarmente emozionante e successo oggi. ho provato un orario diverso dal solito (14:30 invece che 16) forse c'era un po meno gente, oltre questo nessuna differenza.",
+        "",
+        "anche oggi non mi sembra di aver provato particolari emozioni. Mentre tornavo a casa riflettendo mi é sembrato di essere piu rilassato rispetto all'andata",
+        "",
+        ""]
     },
     {
     emozioni: [``,`tristezza (-)`,`soddisfazione (+)`, ``, ``, ``, ``],
-    descrizione: [``, `oggi quando mi sono svegliato mi sentivo un po giu, senza un particolare motivo, forse ho dormito poco/male. Communque niente di che, ero solo un po giu, mi e poi passato nel pomeriggio.`, `soddisfazione legata alla palestra, ho fatto un buon allenamento e inizio ad alzare carichi maggiori.`, `non mi sembra di aver provato particolari emozioni oggi`, `anche oggi mi sento neutro, sto bene, non mi sento triste ma neanche particolarmente felice`, `oggi come ieri`, `oggi sono andato a firenze con mia mamma per comprare cose (abbigliamento) che mi servivano e comunque per fare anche un giro. sono stato bene ma non mi viene da dire di essere stato felice, forse non so "godermi i piccoli piaceri della vita"`]
+    descrizione: [
+        ``,
+        `oggi quando mi sono svegliato mi sentivo un po giu, senza un particolare motivo, forse ho dormito poco/male. Communque niente di che, ero solo un po giu, mi e poi passato nel pomeriggio.`,
+        `soddisfazione legata alla palestra, ho fatto un buon allenamento e inizio ad alzare carichi maggiori.`,
+        `non mi sembra di aver provato particolari emozioni oggi`,
+        `anche oggi mi sento neutro, sto bene, non mi sento triste ma neanche particolarmente felice`,
+        `oggi come ieri`,
+        `oggi sono andato a firenze con mia mamma per comprare cose (abbigliamento) che mi servivano e comunque per fare anche un giro. sono stato bene ma non mi viene da dire di essere stato felice, forse non so "godermi i piccoli piaceri della vita"`]
+    },
+    {
+    emozioni: [``, `contento`, `stanco /n felice`, ``, ``, ``, ``],
+    descrizione: [
+        ``,
+        `di ritorno dall'allenamento, mi sento bene,  sono contento.`,
+        `non esattamente un emozione	ma oggi sento come una senso di stancezza in sottofondo, non stanchezza fisica ne mentale, non mi spiego il  perche ho dormito bene (almeno credo) e appunto non sento una stanchezza fisica. ma se cerco di pensare a come mi sento sento questo leggero senso di stanchezza. Torno dalla palestra ora stanco anche fisicamente ¯\_(ツ)_/¯ ma mi sento contento.`,
+        ``,
+        ``,
+        ``,
+        ``]
+    },
+    {
+    emozioni: [``, ``, ``, ``, ``, ``, ``],
+    descrizione: [
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``]
     }
 ]
 
